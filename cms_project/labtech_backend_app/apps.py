51e11bbe792c8657909f556f1fed2e33ee27941f
MonkeyDSamuel <samueleternityunofficial@gmail.com>
2025-09-18 11:12:22 +0530

    All code committed for creating new branches

---
+++ cms_project/labtech_backend_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LabtechBackendAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'labtech_backend_app'
---
+++ cms_project/receptionist_backend_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReceptionistBackendAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'receptionist_backend_app'
